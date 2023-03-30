30a838ee18d2b096cfe2fbe839a9fbfb4c97c318
Crazy <58318108+CrazyCritic89@users.noreply.github.com>
2023-03-30 09:18:34 -0500

    More Better Deck support
    Works, but still unfinished. Gain deltas not implemented fully but have started to come.

--- main.cpp
+++ main.cpp
@@ -18,9 +18,13 @@
 #define DURATION_MAX        -1
 #define NOTE_STOP           -1
 
-#define DEFAULT_RECLAIM_PERIOD 2
+#define DEFAULT_RECLAIM_PERIOD 280
+
+#define DEFAULT_GAIN 127
 
 using namespace std;
+
+double midiFrequency[128]  = {0, 8.66196, 9.17702, 9.72272, 10.3009, 10.9134, 11.5623, 12.2499, 12.9783, 13.75, 14.5676, 15.4339, 16.3516, 17.3239, 18.354, 19.4454, 20.6017, 21.8268, 23.1247, 24.4997, 25.9565, 27.5, 29.1352, 30.8677, 32.7032, 34.6478, 36.7081, 38.8909, 41.2034, 43.6535, 46.2493, 48.9994, 51.9131, 55, 58.2705, 61.7354, 65.4064, 69.2957, 73.4162, 77.7817, 82.4069, 87.3071, 92.4986, 97.9989, 103.826, 110, 116.541, 123.471, 130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440, 466.164, 493.883, 523.251, 554.365, 587.33, 622.254, 659.255, 698.456, 739.989, 783.991, 830.609, 880, 932.328, 987.767, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.1, 11175.3, 11839.8, 12543.9};
 
 
 struct ParamsStruct{
@@ -29,6 +33,8 @@ struct ParamsStruct{
     int libusbDebugLevel;
     bool repeatSong;
     int reclaimPeriod;
+    unsigned char leftGain;
+    unsigned char rightGain;
 };
 
 struct SteamControllerInfos{
@@ -95,8 +101,6 @@ void SteamController_Close(SteamControllerInfos* controller){
 
 //Steam Controller Haptic Playblack
 int SteamController_PlayNote(SteamControllerInfos* controller, int haptic, int note,double duration = DURATION_MAX){
-    double midiFrequency[128]  = {0, 8.66196, 9.17702, 9.72272, 10.3009, 10.9134, 11.5623, 12.2499, 12.9783, 13.75, 14.5676, 15.4339, 16.3516, 17.3239, 18.354, 19.4454, 20.6017, 21.8268, 23.1247, 24.4997, 25.9565, 27.5, 29.1352, 30.8677, 32.7032, 34.6478, 36.7081, 38.8909, 41.2034, 43.6535, 46.2493, 48.9994, 51.9131, 55, 58.2705, 61.7354, 65.4064, 69.2957, 73.4162, 77.7817, 82.4069, 87.3071, 92.4986, 97.9989, 103.826, 110, 116.541, 123.471, 130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440, 466.164, 493.883, 523.251, 554.365, 587.33, 622.254, 659.255, 698.456, 739.989, 783.991, 830.609, 880, 932.328, 987.767, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.1, 11175.3, 11839.8, 12543.9};
-
     unsigned char dataBlob[64] = {0x8f,
                                   0x00,
                                   0x00, //Trackpad Select: 0x01 = Left, 0x00 = Right
@@ -125,7 +129,7 @@ int SteamController_PlayNote(SteamControllerInfos* controller, int haptic, int n
     //Compute number of repeat. If duration < 0, set to maximum
     uint16_t repeatCount = (duration >= 0.0) ? (duration / period) : 0x7FFF;
 
-    //cout << "Frequency : " <<frequency << ", Period : "<<periodCommand << ", Repeat : "<< repeatCount <<"\n";
+    //cout << "Frequency : " <<frequency << ", Period : "<<periodCommand << ", Repeat : "<< repeatCount <<"\n";S
 
     dataBlob[2] = haptic;
     dataBlob[3] = periodCommand % 0xff;
@@ -146,7 +150,7 @@ int SteamController_PlayNote(SteamControllerInfos* controller, int haptic, int n
 }
 
 //Steam Deck Haptic Playblack - UNFINISHED
-int SteamDeck_PlayNote(SteamControllerInfos* controller, int haptic, int note,double duration = DURATION_MAX){
+int SteamDeck_PlayNote(SteamControllerInfos* controller, int haptic, int note, unsigned char left_gain, unsigned char right_gain, double duration = DURATION_MAX){
 	unsigned char dataBlob[64] = {0xea, //0xEA is the Deck controller's specific haptic command
                                   0x00,
                                   0x00, //Trackpad select : 0x02 = Both, 0x01 = Right, 0x00 = Left
@@ -161,6 +165,24 @@ int SteamDeck_PlayNote(SteamControllerInfos* controller, int haptic, int note,do
                                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	double frequency = midiFrequency[note];
+	uint16_t repeatCount = (note == NOTE_STOP) ? 0x0000 : 0x7FFF;
+
+	dataBlob[2] = haptic;
+	//dataBlob[5] = (haptic == 0) ? left_gain : right_gain;
+	dataBlob[6] = (int)frequency % 0xFF;
+    	dataBlob[7] = (int)frequency / 0xFF;
+    	dataBlob[8] = repeatCount % 0xFF;
+    	dataBlob[9] = repeatCount / 0xFF;
+
+	int r;
+	r = libusb_control_transfer(controller->dev_handle,0x21,9,0x0300,2,dataBlob,64,1000);
+	if(r < 0) {
+		cout<<"Command Error "<<r<< endl;
+		exit(0);
+	}
+
+	return 0;
 }
 
 float timeElapsedSince(std::chrono::steady_clock::time_point tOrigin){
@@ -221,7 +243,7 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
         return;
     }
 
-    //Waiting for user to press enter
+    //Waiting for user to press enter; YOURE WRONG, SULFURIC ACID!
     cout << "Starting playback of " << params.midiSong  << "..." << endl;
     sleep(1);
 
@@ -245,11 +267,11 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
         long currentTick = MidiFile_getTickFromTime(midifile,timeElapsedSince(tOrigin));
 
         //Every reclaimPeriod seconds, reclaim the controller to avoid timeouts
-        if(timeElapsedSince(tRestart) > params.reclaimPeriod){
+        /*if(timeElapsedSince(tRestart) > params.reclaimPeriod){
             tRestart = std::chrono::steady_clock::now();
             SteamController_Close(&steamController1);
             SteamController_Claim(&steamController1);
-        }
+        }*/
 
         //Iterate through all events until the current time, and selecte potential events to play
         for( ; currentEvent != NULL && MidiFileEvent_getTick(currentEvent) < currentTick ; currentEvent = MidiFileEvent_getNextEventInFile(currentEvent)){
@@ -293,7 +315,7 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
             }
 
             //Play notes
-            SteamController_PlayNote(controller,currentChannel,eventNote);
+            if (isDeck) {SteamDeck_PlayNote(controller,currentChannel,eventNote,params.leftGain,params.rightGain);} else {SteamController_PlayNote(controller,currentChannel,eventNote);}
             displayPlayedNotes(currentChannel,eventNote);
         }
     }
@@ -307,9 +329,19 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
 
 bool parseArguments(int argc, char** argv, ParamsStruct* params){
     int c;
-    while ( (c = getopt(argc, argv, "c:l:i:r")) != -1) {
+    while ( (c = getopt(argc, argv, "k:j:c:l:i:r")) != -1) {
         unsigned long int value;
 	switch(c){
+	case 'k':
+	    value = strtoul(optarg,NULL,10);
+            if(value <= 255 && value > 0){
+                params->rightGain = value;
+            }
+	case 'j':
+	    value = strtoul(optarg,NULL,10);
+            if(value <= 255 && value > 0){
+                params->leftGain = value;
+            }
         case 'c':
 	    value = strtoul(optarg,NULL,10);
             if(value <= 1000000 && value > 0){
@@ -362,7 +394,7 @@ void abortPlaying(int){
 
 int main(int argc, char** argv)
 {
-    cout <<"Steam Controller Singer by Pila, Deck support by Crazy"<<endl;
+    cout <<"Steam Haptics Singer by Crazy, Steam Controller Singer by Pila"<<endl;
 
     ParamsStruct params;
     params.intervalUSec = DEFAULT_INTERVAL_USEC;
@@ -370,11 +402,13 @@ int main(int argc, char** argv)
     params.repeatSong = false;
     params.midiSong = "\0";
     params.reclaimPeriod = DEFAULT_RECLAIM_PERIOD;
+    params.leftGain = DEFAULT_GAIN;
+    params.rightGain = DEFAULT_GAIN;
 
 
     //Parse arguments
     if(!parseArguments(argc, argv, &params)){
-        cout << "Usage : steam-haptics-singer [-r][-lDEBUG_LEVEL] [-iINTERVAL] [-cRECLAIM_PERIOD] MIDI_FILE" << endl;
+        cout << "Usage: steam-haptics-singer [-r][-l DEBUG_LEVEL] [-i INTERVAL] [-c RECLAIM_PERIOD] [-j LEFTGAIN] [-k RIGHTGAIN] MIDI_FILE" << endl;
         return 1;
     }
 
