c9b0f9eae4d0732220a65b94d3368f2a1784eca6
CrazyCritic89 <cxrx5000@gmail.com>
2026-07-21 23:06:47 -0500

    Detect between Steam Deck LCD or OLED, fixed building

--- main.cpp
+++ main.cpp
@@ -31,14 +31,15 @@
 #define STEAM_PUCK				0x1304
 #define STEAM_DECK				0x1205
 
-double midiFrequency[128]  = {0, 8.662, 9.177, 9.723, 10.301, 10.913, 11.562, 12.250, 12.978, 13.750, 14.568, 15.434, 16.352, 17.324, 18.354, 19.445, 20.602, 21.827, 23.125, 24.500, 25.957, 27.500, 29.135, 30.868, 32.703, 34.648, 36.708, 38.891, 41.203, 43.654, 46.249, 48.999, 51.913, 55.000, 58.270, 61.735, 65.406, 69.296, 73.416, 77.782, 82.407, 87.307, 92.499, 97.999, 103.826, 110.000, 116.541, 123.471, 130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220.000, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440.000, 466.164, 493.883, 523.251, 554.365, 587.330, 622.254, 659.255, 698.456, 739.989, 783.991, 830.609, 880.000, 932.328, 987.767, 1046.502, 1108.731, 1174.659, 1244.508, 1318.510, 1396.913, 1479.978, 1567.982, 1661.219, 1760.000, 1864.655, 1975.533, 2093.005, 2217.461, 2349.318, 2489.016, 2637.020, 2793.826, 2959.955, 3135.963, 3322.438, 3520.000, 3729.310, 3951.066, 4186.009, 4434.922, 4698.636, 4978.032, 5274.041, 5587.652, 5919.911, 6271.927, 6644.875, 7040.000, 7458.620, 7902.133, 8372.018, 8869.844, 9397.273, 9956.063, 10548.082, 11175.303, 11839.822, 12543.854};
-uint16_t midiFrequencyDk[128] = {440};
-uint16_t midiFrequencyRb[128] = {0, 10, 10, 11, 11, 12, 13, 13, 14, 15, 16, 16, 17, 18, 19, 20, 22, 23, 24, 25, 27, 29, 30, 32, 34, 36, 38, 40, 42, 45, 47, 50, 53, 56, 59, 63, 66, 70, 75, 80, 84, 89, 94, 100, 107, 113, 120, 126, 134, 142, 151, 160, 169, 179, 189, 200, 213, 226, 239, 253, 267, 283, 300, 318, 336, 357, 377, 399, 423, 449, 477, 505, 535, 566, 598, 636, 674, 713, 756, 800, 848, 898, 951, 1008, 1068, 1131, 1199, 1270, 1345, 1425, 1510, 1600, 1693, 1792, 1897, 2008, 2125, 2249, 2381, 2521, 2669, 2826, 2992, 3168, 3354, 3552, 3761, 3983, 4218, 4467, 4731, 5010, 5306, 5620, 5952, 6304, 6677, 7072, 7491, 7934, 8404, 8902, 9429, 9988, 10580, 11207, 11872, 12576};
-uint16_t midiFrequencyTr[128] = {0, 9, 9, 10, 10, 11, 12, 12, 13, 14, 15, 15, 16, 17, 18, 19, 21, 22, 23, 24, 26, 28, 29, 31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 261, 276, 293, 310, 328, 349, 369, 391, 414, 439, 466, 493, 522, 552, 584, 621, 658, 696, 738, 781, 828, 877, 929, 985, 1043, 1105, 1171, 1240, 1314, 1392, 1475, 1562, 1655, 1754, 1858, 1969, 2085, 2209, 2340, 2480, 2627, 2784, 2949, 3124, 3311, 3507, 3716, 3938, 4173, 4422, 4686, 4965, 5261, 5575, 5907, 6259, 6632, 7027, 7446, 7889, 8359, 8857, 9384, 9943, 10535, 11162, 11827, 12531};
+const double midiFrequency[128]  = {0, 8.662, 9.177, 9.723, 10.301, 10.913, 11.562, 12.250, 12.978, 13.750, 14.568, 15.434, 16.352, 17.324, 18.354, 19.445, 20.602, 21.827, 23.125, 24.500, 25.957, 27.500, 29.135, 30.868, 32.703, 34.648, 36.708, 38.891, 41.203, 43.654, 46.249, 48.999, 51.913, 55.000, 58.270, 61.735, 65.406, 69.296, 73.416, 77.782, 82.407, 87.307, 92.499, 97.999, 103.826, 110.000, 116.541, 123.471, 130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220.000, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440.000, 466.164, 493.883, 523.251, 554.365, 587.330, 622.254, 659.255, 698.456, 739.989, 783.991, 830.609, 880.000, 932.328, 987.767, 1046.502, 1108.731, 1174.659, 1244.508, 1318.510, 1396.913, 1479.978, 1567.982, 1661.219, 1760.000, 1864.655, 1975.533, 2093.005, 2217.461, 2349.318, 2489.016, 2637.020, 2793.826, 2959.955, 3135.963, 3322.438, 3520.000, 3729.310, 3951.066, 4186.009, 4434.922, 4698.636, 4978.032, 5274.041, 5587.652, 5919.911, 6271.927, 6644.875, 7040.000, 7458.620, 7902.133, 8372.018, 8869.844, 9397.273, 9956.063, 10548.082, 11175.303, 11839.822, 12543.854};
+const uint16_t midiFrequencyDk[128] = {440};
+const uint16_t midiFrequencyRb[128] = {0, 10, 10, 11, 11, 12, 13, 13, 14, 15, 16, 16, 17, 18, 19, 20, 22, 23, 24, 25, 27, 29, 30, 32, 34, 36, 38, 40, 42, 45, 47, 50, 53, 56, 59, 63, 66, 70, 75, 80, 84, 89, 94, 100, 107, 113, 120, 126, 134, 142, 151, 160, 169, 179, 189, 200, 213, 226, 239, 253, 267, 283, 300, 318, 336, 357, 377, 399, 423, 449, 477, 505, 535, 566, 598, 636, 674, 713, 756, 800, 848, 898, 951, 1008, 1068, 1131, 1199, 1270, 1345, 1425, 1510, 1600, 1693, 1792, 1897, 2008, 2125, 2249, 2381, 2521, 2669, 2826, 2992, 3168, 3354, 3552, 3761, 3983, 4218, 4467, 4731, 5010, 5306, 5620, 5952, 6304, 6677, 7072, 7491, 7934, 8404, 8902, 9429, 9988, 10580, 11207, 11872, 12576};
+const uint16_t midiFrequencyTr[128] = {0, 9, 9, 10, 10, 11, 12, 12, 13, 14, 15, 15, 16, 17, 18, 19, 21, 22, 23, 24, 26, 28, 29, 31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 261, 276, 293, 310, 328, 349, 369, 391, 414, 439, 466, 493, 522, 552, 584, 621, 658, 696, 738, 781, 828, 877, 929, 985, 1043, 1105, 1171, 1240, 1314, 1392, 1475, 1562, 1655, 1754, 1858, 1969, 2085, 2209, 2340, 2480, 2627, 2784, 2949, 3124, 3311, 3507, 3716, 3938, 4173, 4422, 4686, 4965, 5261, 5575, 5907, 6259, 6632, 7027, 7446, 7889, 8359, 8857, 9384, 9943, 10535, 11162, 11827, 12531};
 
-int8_t gainCurveDk[128] = {DEFAULT_GAIN};
-int8_t gainCurveRb[128] = {DEFAULT_GAIN};
-int8_t gainCurveTr[128] = {DEFAULT_GAIN};
+const int8_t gainCurveJp[128] = {DEFAULT_GAIN};
+const int8_t gainCurveGa[128] = {DEFAULT_GAIN};
+const int8_t gainCurveRb[128] = {DEFAULT_GAIN};
+const int8_t gainCurveTr[128] = {DEFAULT_GAIN};
 
 struct ParamsStruct{
 	const char* midiSong;
@@ -61,7 +62,7 @@ enum class ControllerType {
 	Original,	//Steam Controller (2015)
 	Triton,		//Steam Controller (2026)
 	Jupiter, 	//Steam Deck (LCD)
-	Galileo 	//Steam Deck (OLED)
+	//Galileo 	//Steam Deck (OLED)
 };
 
 struct SteamControllerInfos{
@@ -69,6 +70,7 @@ struct SteamControllerInfos{
 	hid_device* hid_handle = NULL;
 	int interfaceNum = 0;
 	ControllerType type = ControllerType::None;
+	bool isOled = false;
 };
 
 SteamControllerInfos steamController1;
@@ -76,7 +78,10 @@ SteamControllerInfos steamController1;
 hid_device* open_steam_controller_hid(uint16_t pid) {
 	unsigned char buf[64];
 	struct hid_device_info *devs = hid_enumerate(VALVE_VID, pid);
-	if (devs != NULL) std::cout << "Attempting to find Steam Controller (2026)..."<<std::endl;
+	if (devs == NULL) return NULL;
+	if (pid == STEAM_CONTROLLER_2026) std::cout << "\rAttempting to find wired Steam Controller (2026)... ";
+	else if (pid == STEAM_PUCK) std::cout << "\rFound Steam Puck, attempting to find first Steam Controller (2026)... ";
+	else std::cout << "\rAttempting to find Valve device...";
 	hid_device* handle = NULL;
 	int r;
 	for (struct hid_device_info *cur = devs; cur != NULL; cur = cur->next) {
@@ -95,77 +100,63 @@ hid_device* open_steam_controller_hid(uint16_t pid) {
 	return handle;
 }
 
+bool Check_SteamDeckOLED() {
+	std::ifstream file("/sys/class/dmi/id/product_name");
+	char buffer[8];
+	if (!file) {
+		return false;
+	}
+	while(file.getline(buffer, sizeof(buffer))) {
+		if( strncmp(buffer,"Galileo",sizeof(buffer))==0 ) return true;
+	}
+	return false;
+}
+
 bool SteamController_Open(SteamControllerInfos* controller){
 	if(!controller) return false;
 
 	struct hid_device_info *devs, *cur_dev;
 	//Open Steam Controller device
 	if((controller->dev_handle = libusb_open_device_with_vid_pid(NULL, VALVE_VID, STEAM_CONTROLLER)) != NULL){ // A Steam Controller
-		std::cout<<"Found a Steam Controller"<<std::endl;
+		std::cout<<"Found a Steam Controller\n";
 		controller->interfaceNum = 2;
 		controller->type = ControllerType::Original;
 	}
 	else if((controller->dev_handle = libusb_open_device_with_vid_pid(NULL, VALVE_VID, STEAM_CONTROLLER_2015)) != NULL){ // Wired Steam Controller (2015)
-		std::cout<<"Found wired Steam Controller (2015)"<<std::endl;
+		std::cout<<"Found wired Steam Controller (2015)\n";
 		controller->interfaceNum = 2;
 		controller->type = ControllerType::Original;
 	}
 	else if((controller->dev_handle = libusb_open_device_with_vid_pid(NULL, VALVE_VID, STEAM_DONGLE)) != NULL){ // Steam Controller (2015) dongle //TODO: FIX
-		std::cout<<"Found Steam Dongle, will attempt to use the first Steam Controller (2015)"<<std::endl;
+		std::cout<<"Found Steam Dongle, will attempt to use the first Steam Controller (2015)\n";
 		controller->interfaceNum = 1;
 		controller->type = ControllerType::Original;
 	} 
 	else if((controller->hid_handle = open_steam_controller_hid(STEAM_CONTROLLER_2026)) != NULL) { // Steam Controller (2026)
-		std::cout<<"Found wired Steam Controller (2026)"<<std::endl;
+		std::cout<<"OK\n";
 		controller->type = ControllerType::Triton;
 		if (!tritonLimit) channelCount = 4;
 	}
 	else if((controller->hid_handle = open_steam_controller_hid(STEAM_PUCK)) != NULL) { // Steam Puck
-		std::cout<<"Found Steam Puck, using first Steam Controller (2026)"<<std::endl;
+		std::cout<<"OK\n";
 		controller->type = ControllerType::Triton;
 		if (!tritonLimit) channelCount = 4;
 	}
 	else if((controller->dev_handle = libusb_open_device_with_vid_pid(NULL, VALVE_VID, STEAM_DECK)) != NULL){ // Steam Deck
-		std::cout<<"Found Steam Deck"<<std::endl;
+		std::cout<<"Found Steam Deck ";
 		controller->interfaceNum = 2;
 		controller->type = ControllerType::Jupiter;
+		if(Check_SteamDeckOLED()) {
+			std::cout<<"(OLED)\n";
+			controller->isOled = true;
+		}
+		else std::cout << "(LCD)\n";
 	}
 	else{
 		std::cout<<"No device found"<<std::endl;
 		std::cin.ignore();
 		return false;
 	}
-
-	//Load gain curves
-	std::ifstream file1,file2;
-	if (!noGainCurve) {
-		switch (controller->type) {
-			case ControllerType::Triton:
-				file1.open("gaincurve/Triton_Trackpads.txt");
-				file2.open("gaincurve/Triton_Rumble.txt");
-				if(!file1 || !file2) {
-					std::cout << "Could not open gain curve, defaulting to 0" << std::endl;
-					break;
-				}
-				for (int i = 0; i < 128; ++i) {
-					file1 >> gainCurve[i];
-					file2 >> gainCurveRb[i];
-				}
-				break;
-			case ControllerType::Jupiter:
-				file1.open("gaincurve/Jupiter.txt");
-				if(!file1) {
-					std::cout << "Could not open gain curve, defaulting to 0" << std::endl;
-					break;
-				}
-				for (int i = 0; i < 128; ++i) {
-					file1 >> gainCurve[i];
-				}
-				break;
-		}
-	}
-	file1.close();
-	file2.close();
 
 	//If dev_handle is NULL, it's using HIDAPI so skip this
 	if(controller->dev_handle != NULL) {
@@ -224,7 +215,7 @@ int SteamHaptics_PlayNote(SteamControllerInfos* controller, int channel, int not
 		if (note == NOTE_STOP) {
 			dataBlob[0] = 0x8F;
 			dataBlob[2] = channel;
-			dataBlob[8] = 0x80;
+			//dataBlob[8] = 0x80;
 		} else {
 			//period = 1.0 / frequency;
 			periodCommand = STEAM_CONTROLLER_MAGIC_PERIOD_RATIO / frequency; //Reminder to check if the Steam Controller tuning lines up with the Deck.
@@ -265,7 +256,7 @@ int SteamHaptics_PlayNote(SteamControllerInfos* controller, int channel, int not
 		} else {
 			//Get frequency and gain needed depending on haptic
 			freq = (haptic > 2) ? midiFrequencyRb[note] : midiFrequencyTr[note];
-			gain = (haptic > 2) ? gainCurveRb[note] : gainCruveTr[note];
+			gain = (haptic > 2) ? gainCurveRb[note] : gainCurveTr[note];
 			dataBlob[0] = 0x83;
 			dataBlob[1] = haptic;
 			dataBlob[2] = ((directVel) ? (velocity * 255) / 127 - 128 : gain) + gainModifier[haptic];
@@ -293,11 +284,11 @@ int SteamHaptics_PlayNote(SteamControllerInfos* controller, int channel, int not
 			dataBlob[5] = 0x80;
 			dataBlob[9] = 0x80;
 		} else {
-			freq = midiFrequencyDk[note];
+			freq = midiFrequency[note];
 			dataBlob[0] = 0xEA;
 			dataBlob[2] = !channel; //Swap haptics to match 2015
 			dataBlob[3] = 0x03; 
-			dataBlob[5] = ((directVel) ? (velocity * 255) / 127 - 128 : gainCurveDk[note]) + gainModifier[!channel];
+			dataBlob[5] = ((directVel) ? (velocity * 255) / 127 - 128 : gainCurveJp[note]) + gainModifier[!channel];
 			dataBlob[6] = freq % 0xFF;
 			dataBlob[7] = freq / 0xFF;
 			dataBlob[8] = 0xFF;
@@ -381,7 +372,7 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
 		directVel = true;
     }
 	if (strstr(params.midiSong,"_dc")) {
-		std::cout << "THIS FILE MAY BE FOR A NEWER VERSION OF STEAM HAPTICS SINGER! Found \"_dv\" in file name, assuming direct velocity to gain control"
+		std::cout << "THIS FILE MAY BE FOR A NEWER VERSION OF STEAM HAPTICS SINGER! Found \"_dv\" in file name, assuming direct velocity to gain control";
 		directVel = true;
 	}
 
